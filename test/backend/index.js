26d52b3a0105a843a7a76141377e7d6819e8ca79
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-08-20 21:39:25 +0530

    MongoDB  NodeJS, Connect

--- test/backend/index.js
+++ test/backend/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
-app.get(
-    "/",
-    (req, res) => {
-        res.send("Hello World")
-    }
-)
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://dila:dila1234@cluster0.agoys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(() => {
+    console.log("Failed");
+  });
 
 app.listen(5000, () => console.log("Backend is Running"));
