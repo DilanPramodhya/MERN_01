52cc1733d9d72cb6ffd056715142008e1781477d
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-08-20 18:54:19 +0530

    Run ReactJS and NodeJS in One Terminal - Concurrently

--- test/backend/index.js
+++ test/backend/index.js
@@ -1,1 +1,12 @@
-const express = require('express')
+const express = require("express");
+
+const app = express();
+
+app.get(
+    "/",
+    (req, res) => {
+        res.send("Hello World")
+    }
+)
+
+app.listen(5000, () => console.log("Backend is Running"));
